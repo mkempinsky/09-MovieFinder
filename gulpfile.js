2cb8eb300189062addae3c7b67ffa49e4a900376
Ryan Dowdy <rdowdy21@gmail.com>
2016-08-16 16:43:26 -0700

    Change to materialize. Put search results in cards.

--- gulpfile.js
+++ gulpfile.js
@@ -50,7 +50,7 @@ gulp.task('connect', function() {
 gulp.task('app', function(){
     var options = {
         uri: 'http://localhost:8080',
-        app: 'Google Chrome'
+        app: 'chrome'
     };
     gulp.src('./app/index.html')
         .pipe(open(options));
