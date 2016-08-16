62a9fc8ab36cb3b4f2701d017370e346adb84a64
Michele Kempinsky <mkempinsky@icloud.com>
2016-08-16 14:44:50 -0700

    front-end started

---
+++ app/js/search.controller.js
@@ -0,0 +1,17 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('searchController', searchController);
+
+    Controller.$inject = ['dependencies'];
+
+    /* @ngInject */
+    function searchController() {
+        var vm = this;
+        vm.movieSearch= "";
+}
+
+       
+})();
