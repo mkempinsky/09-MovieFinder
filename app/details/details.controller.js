f5a79ba4e1005622447699905373066466d25f10
Michele Kempinsky <mkempinsky@icloud.com>
2016-08-17 09:37:06 -0700

    added details controller

---
+++ app/details/details.controller.js
@@ -0,0 +1,15 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('detailController', detailController);
+
+    dletailController.$inject = ['$stateParams'];
+
+    /* @ngInject */
+    function detailController($stateParams) {
+        var vm = this;
+      }
+
+})();
